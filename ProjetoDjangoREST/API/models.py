cd79351201845116a0a42502a8714593606886e5
Ryan Nascimento <ryandonascimentobezerra@gmail.com>
2024-08-21 17:07:32 -0300

    Refino API

--- ProjetoDjangoREST/API/models.py
+++ ProjetoDjangoREST/API/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from uuid import uuid4
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 
@@ -10,5 +11,17 @@ class Jogos(models.Model):
     Desenvolvedora = models.CharField(max_length=255)
     Versão = models.CharField(max_length=255)
     Categoria = models.CharField(max_length=255)
-    Média_Avaliações = models.CharField(max_length=255)
-    Média_Horas_Jogando = models.CharField(max_length=255)
+    Média_Avaliações = models.FloatField(
+        validators=[
+            MinValueValidator(0.0),  
+            MaxValueValidator(10.0)
+        ]
+    )
+    Média_Horas_Jogando = models.DecimalField(
+        max_digits=5, 
+        decimal_places=0,
+        validators=[
+            MinValueValidator(0)
+        ]
+    )
+

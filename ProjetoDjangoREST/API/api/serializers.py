a7f554cb0be7151a0163e91ee3c5008416671491
Ryan Bezerra <101473672+RyanBezerra@users.noreply.github.com>
2024-08-23 17:32:22 -0300

    🗑️ remove: Removendo arquivos não utilizados do projeto

--- ProjetoDjangoREST/API/api/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from API import models
-
-class JogosSerializers(serializers.ModelSerializer):
-    class Meta:
-        model = models.Jogos
-        fields = '__all__'

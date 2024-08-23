ca6b866ca09ca546b86c61220c38e621811ce600
Ryan Bezerra <101473672+RyanBezerra@users.noreply.github.com>
2024-08-23 17:31:54 -0300

    🗑️ remove: Removendo arquivos não utilizados do projeto

--- ProjetoBibliotecaREST/Apps/Livro/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import LivroModel
-
-"""criação do serializers da model Livro"""
-
-class LivroSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = LivroModel
-        fields = '__all__'
--- ProjetoBibliotecaREST/Apps/Livro/viewsets.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import viewsets
-from .models import LivroModel
-from .serializers import LivroSerializer
-
-class LivroViewSet(viewsets.ModelViewSet):
-    queryset = LivroModel.objects.all()
-    serializer_class = LivroSerializer
